2b49a93ac55e29306b16ef3f89162bca950592b5
ccchangkong <chlo0o@qq.com>
2016-10-27 16:32:44 +0800

    create

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+    <sidebar></sidebar>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Sidebar from './components/Sidebar'
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    Sidebar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sidebar">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
