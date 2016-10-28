8eaccf76e27bb53b5630ac6a4e59a05f02b2482d
ccchangkong <chlo0o@qq.com>
2016-10-28 18:02:17 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
+Vue.use(Element)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
