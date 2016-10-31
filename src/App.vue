0138ed9e9d4156ed5c334d1d7393c190961be13b
ccchangkong <chlo0o@qq.com>
2016-10-31 18:00:51 +0800

    add
    父子传值，本地存储

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
+  <p>{{values}}</p>
     <sidebar></sidebar>
-    <subject></subject>   
+    <subject :valuess.sync="values" @onInput="onInputs"></subject>   
   </div>
 </template>
 
@@ -10,9 +11,33 @@ import Sidebar from './components/Sidebar'
 import Subject from './components/Subject'
 export default {
   name: 'app',
+  data () {
+    return {
+      values: 3,
+      resume: JSON.parse(window.localStorage.getItem('resume')) || []
+    }
+  },
   components: {
     Sidebar,
     Subject
+  },
+  methods: {
+    onInputs: function () {
+      this.values += 1
+      var data = {
+        value: this.values,
+        time: window.Date.now()
+      }
+      this.resume.push(data)
+    }
+  },
+  watch: {
+    resume: {
+      handler: function (resume) {
+        window.localStorage.setItem('resume', window.JSON.stringify(resume))
+      },
+      deep: true
+    }
   }
 }
 </script>
@@ -24,6 +49,9 @@ export default {
     font-size: 0;
     box-shadow: 0 0 5px #333;
 
+}
+p{
+  font-size: 10px;
 }
   .sidebar{
     display: inline-block;
--- src/components/Subject.vue
+++ src/components/Subject.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="subject">
-<h1>{{ msg }}</h1>
-<div class="block">
-  <span class="demonstration">默认不区分颜色</span>
-  <el-rate v-model="value1"></el-rate>
-</div>
+    <h1>{{ msg }}</h1>
+    <div class="block">
+      <p @click="onInput">{{valuess}}</p>
+      <span class="demonstration">默认不区分颜色</span>
+      <el-rate v-model="valuess"></el-rate>
+    </div>
   </div>
-
 </template>
 
 <script>
 export default {
   name: 'subject',
+  props: ['valuess'],
+  methods: {
+    onInput: function () {
+      this.$emit('onInput')
+    }
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      value1: 4
+      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
