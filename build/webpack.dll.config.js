3c3e3ff78f9614f5c76db61f6273c8d22c1df1b1
ccchangkong <chlo0o@qq.com>
2018-01-18 08:07:15 +0800

    Html2canvas dll打包

--- build/webpack.dll.config.js
+++ build/webpack.dll.config.js
@@ -4,7 +4,7 @@ var webpack = require("webpack");
 module.exports = {
   // 你想要打包的模块的数组
   entry: {
-    vendor: ['vue/dist/vue.esm.js']
+    vendor: ['vue/dist/vue.esm.js','Html2canvas']
   },
   output: {
     path: path.join(__dirname, '../static/js'), // 打包后文件输出的位置
